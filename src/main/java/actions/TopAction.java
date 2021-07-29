ba78d7468984a129f698ad699ce4b3a21e73f135
yusuke.iga <yusuke_iga@r.recruit.co.jp>
2021-07-29 15:57:49 +0900

    トップページアクションの作成

--- src/main/java/actions/TopAction.java
+++ src/main/java/actions/TopAction.java
@@ -1,11 +1,16 @@
 package actions;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 
+import actions.views.EmployeeView;
+import actions.views.ReportView;
 import constants.AttributeConst;
 import constants.ForwardConst;
+import constants.JpaConst;
+import services.ReportService;
 
 /**
  * トップページに関する処理を行うActionクラス
@@ -13,14 +18,20 @@ import constants.ForwardConst;
  */
 public class TopAction extends ActionBase {
 
+    private ReportService service;
+
     /**
      * indexメソッドを実行する
      */
     @Override
     public void process() throws ServletException, IOException {
 
+        service = new ReportService();
+
         //メソッドを実行
         invoke();
+
+        service.close();
 
     }
 
@@ -29,6 +40,21 @@ public class TopAction extends ActionBase {
      */
 
     public void index() throws ServletException, IOException {
+
+        //セッションからログイン中の従業員情報を取得
+        EmployeeView loginEmployee  = (EmployeeView)getSessionScope(AttributeConst.LOGIN_EMP);
+
+        //ログイン中の従業員が作成した日報を、指定されたページ数の一覧画面に表示する分取得する
+        int page = getPage();
+        List<ReportView> reports = service.getMinePerPage(loginEmployee , page);
+
+        //ログイン中の従業員が作成した日報データの件数を取得
+        long myReportsCount = service.countAllMine(loginEmployee);
+
+        putRequestScope(AttributeConst.REPORTS, reports); //取得した日報データ
+        putRequestScope(AttributeConst.REP_COUNT, myReportsCount); //ログイン中の従業員が作成した日報の数
+        putRequestScope(AttributeConst.PAGE, page); //ページ数
+        putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE); //1ページに表示するレコードの数
 
         //セッションにフラッシュメッセージが設定されている場合は
         //リクエストスコープに移し替え、セッションからは削除する
